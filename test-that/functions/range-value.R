6bad78b0678c5b884ef0f22f31bd3eaf43918c70
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-10 13:10:56 -0700

    improved range-value functions

--- test-that/functions/range-value.R
+++ test-that/functions/range-value.R
@@ -1,6 +1,17 @@
 #' @title Range Value
 #' @param x numeric vector
 #' @return range as max - min
-range_value <- function(x) {
-  max(x) - min(x)
+
+range_value <- function(x, na.rm) {
+  x = c(1,2,3,4,5, NA)
+  
+  if(na.rm == 'TRUE')
+  {
+    x = x[!is.na(x)]
+  }
+  elseif(na.rm=='FALSE')
+  {
+    max(x) - min(x)  
+  }
+
 }
