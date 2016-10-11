45977a6ac868033c8d6d2dea8fda31f275a0acb3
PhilHoon Oh <vlfgns5@gmail.com>
2016-10-10 21:42:23 -0700

    using return for readability

--- test-that/functions/extra.R
+++ test-that/functions/extra.R
@@ -3,6 +3,7 @@
 #' @return a numeric vector with median and mean
 center_measures = function(x) {
   out = c(mean(x),median(x))
+  return(out)
 }
 
 #' @title variability measure
@@ -10,6 +11,7 @@ center_measures = function(x) {
 #' @return a numeric vector with range, iqr, and stdev
 spread_measures = function(x){
   out = c((max(x)-min(x)), IQR(x), sd(x))
+  return(out)
 }
 
 #' @title descriptive statistics
@@ -19,5 +21,6 @@ spread_measures = function(x){
 descriptive_stats = function(x){
   out = c(median(x, na.rm = 1), mean(x, na.rm = 1), (max(x, na.rm = 1)-min(x, na.rm = 1)),
           IQR(x, na.rm = 1), sd(x, na.rm = 1), sum(is.na(x)))
+  return(out)
 }
 
